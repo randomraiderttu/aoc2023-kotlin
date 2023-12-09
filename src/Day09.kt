570839b7c6f8f7931070246ba422b7432c87b3f4
Brad Hudson <bradly.hudson@gmail.com>
2023-12-09 01:38:57 -0600

    Day 8 Solution

---
+++ src/Day09.kt
@@ -0,0 +1,61 @@
+fun getSequence(input: List<Int>, isPart2: Boolean): Int {
+    val tmpSequence = mutableListOf<Int>()
+
+    if (input.size != input.filter { it == 0 }.size) {
+
+        for (i in 0..<input.size-1) {
+            tmpSequence.add(input[i+1] - input[i])
+        }
+
+        if (isPart2) {
+            return input[0] - getSequence(tmpSequence, isPart2)
+        } else {
+            return input[input.size - 1] + getSequence(tmpSequence, isPart2)
+        }
+    } else {
+        return 0
+    }
+}
+
+fun loadSequences(input: List<String>): MutableList<List<Int>> {
+    var tmpList: MutableList<List<Int>> = mutableListOf()
+    for (rec in input) {
+        var strList = rec.split("\\s+".toRegex())
+        var intList = strList.map{it.toInt()}
+        tmpList.add(intList)
+    }
+
+    return tmpList
+}
+fun solvePart1Day09(input: MutableList<List<Int>>): Int {
+    var total = 0
+
+    for (rec in input) {
+        total += getSequence(rec, false)
+    }
+
+    return total
+}
+
+fun solvePart2Day09(input: MutableList<List<Int>>): Int {
+    var total = 0
+
+    for (rec in input) {
+        total += getSequence(rec, true)
+    }
+
+    return total
+}
+
+fun main() {
+//    val input = readInput("Day09_test")
+    val input = readInput("Day09_real")
+
+    val seqList = loadSequences(input)
+
+    val firstResult = solvePart1Day09(seqList)
+    val secondResult = solvePart2Day09(seqList)
+
+    println("First Result: $firstResult")
+    println("Second Result: $secondResult")
+}
